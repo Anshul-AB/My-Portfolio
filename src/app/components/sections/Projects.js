19ac6114d0d2a802e6d15e8a71288fd0b61ea9e8
anshul-999 <anshul.b100@gmail.com>
2026-02-21 14:24:30 +0530

    fix slider ui

--- src/app/components/sections/Projects.js
+++ src/app/components/sections/Projects.js
@@ -93,7 +93,9 @@ export default function Home() {
       dockerLink: "https://hub.docker.com/r/anshul100/vibing-music-app",
     },
   ];
-const settings = {
+
+  const settings = {
+  mobileFirst: true,
   dots: true,
   infinite: true,
   speed: 800,
@@ -101,21 +103,14 @@ const settings = {
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
-
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
+      breakpoint: 768,
+      settings: { slidesToShow: 1 },
     },
     {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
+      breakpoint: 1024,
+      settings: { slidesToShow: 2 },
     },
   ],
 };
